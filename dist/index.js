9004d7710ea05044470aa1e2ebff3878edf1c40d
Sodikin <sodikinnaa@users.noreply.github.com>
2026-03-03 12:38:52 +0700

    fix: switch compilation to ESM (NodeNext) to resolve ReferenceError exports in v1.0.3

--- dist/index.js
+++ dist/index.js
@@ -1,49 +1,10 @@
-"use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || (function () {
-    var ownKeys = function(o) {
-        ownKeys = Object.getOwnPropertyNames || function (o) {
-            var ar = [];
-            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
-            return ar;
-        };
-        return ownKeys(o);
-    };
-    return function (mod) {
-        if (mod && mod.__esModule) return mod;
-        var result = {};
-        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
-        __setModuleDefault(result, mod);
-        return result;
-    };
-})();
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = register;
 /**
  * Smart Report MCP Plugin for OpenClaw
  */
-const axios_1 = __importDefault(require("axios"));
-const fs = __importStar(require("fs"));
-const path = __importStar(require("path"));
-const os = __importStar(require("os"));
+import axios from 'axios';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as os from 'os';
 const PLUGIN_ID = 'smart-report-plugin';
 const API_BASE = 'https://smartreport.siapdigital.my.id/api/mcp';
 function saveConfig(token) {
@@ -70,7 +31,7 @@ async function callMcp(api, method, params = {}) {
     if (!token) {
         throw new Error('API Token not found. Please run "openclaw smart-auth <token>" first.');
     }
-    const response = await axios_1.default.post(API_BASE, {
+    const response = await axios.post(API_BASE, {
         jsonrpc: '2.0',
         method: method,
         params: params,
@@ -87,7 +48,7 @@ async function callMcp(api, method, params = {}) {
     }
     return response.data.result;
 }
-function register(api) {
+export default function register(api) {
     // 1. CLI Commands
     api.registerCli(({ program }) => {
         program
